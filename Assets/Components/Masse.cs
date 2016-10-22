9dc810a1d6423128e6f4e4da59374b608bb3092e
Thibault Gigant <thibault.gigant@gmail.com>
2016-10-22 14:57:36 +0200

    Ajout Masse et Modification formule Gravity

---
+++ Assets/Components/Masse.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Masse : MonoBehaviour {
+	// Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
+	public float mass;
+	public int exposant;
+}
--- Assets/Systems/GravitySystem.cs
+++ Assets/Systems/GravitySystem.cs
@@ -3,11 +3,11 @@ using FYFY;
 
 public class GravitySystem : FSystem {
 	// Récupération des familles sur lesquelles agit le système
-	private Family orbiteurs = FamilyManager.getFamily(new AllOfComponents(typeof(Orbiteur), typeof(Rigidbody)));
-	private Family attracteurs = FamilyManager.getFamily(new AllOfComponents(typeof(Attracteur), typeof(Rigidbody)));
+	private Family orbiteurs = FamilyManager.getFamily(new AllOfComponents(typeof(Orbiteur), typeof(Masse)));
+	private Family attracteurs = FamilyManager.getFamily(new AllOfComponents(typeof(Attracteur), typeof(Masse)));
 
 	// Constante gravitationnelle pour les calculs des forces
-	public static float GRAVITY_CNST = 6.67384e-8f; // ATTENTION : constante modifiée pour masses en tonnes
+	public static float GRAVITY_CNST = 6.67384e-11f;
 
 	// Use this to update member variables when system pause. 
 	// Advice: avoid to update your families inside this function.
@@ -21,24 +21,25 @@ public class GravitySystem : FSystem {
 
 	// Use to process your families.
 	protected override void onProcess(int familiesUpdateCount) {
-		float masseOrbiteur; // Masse de l'orbiteur courant dans la boucle
-		float masseAttracteur; // Masse de l'attracteur courant dans la boucle
+		double masseOrbiteur; // Masse de l'orbiteur courant dans la boucle
+		double masseAttracteur; // Masse de l'attracteur courant dans la boucle
 		Rigidbody rb; // Rigidbody de l'attracteur courant dans la boucle
-		float force; // Force que l'on ajoutera à celles qui s'appliquent déjà sur le Rigidbody de l'orbiteur
+		double force; // Force que l'on ajoutera à celles qui s'appliquent déjà sur le Rigidbody de l'orbiteur
 		Vector3 direction; // Direction de la force qui s'appliquera
 
 		// Pour chaque orbiteur
 		foreach (GameObject orbiteur in orbiteurs) {
 			rb = orbiteur.GetComponent<Rigidbody>();
-			masseOrbiteur = rb.mass;
+			Masse componentMasseOrbiteur = orbiteur.GetComponent<Masse> ();
+			masseOrbiteur = componentMasseOrbiteur.mass * Mathf.Pow(10, componentMasseOrbiteur.exposant);
 			// On lui applique la force de gravité de chaque attracteur
 			foreach (GameObject attracteur in attracteurs) {
-				masseAttracteur = attracteur.GetComponent<Rigidbody> ().mass;
+				masseAttracteur = attracteur.GetComponent<Masse>().mass * Mathf.Pow(10, attracteur.GetComponent<Masse>().exposant);
 				direction = ( attracteur.transform.position - orbiteur.transform.position ).normalized ;
 
-				force = (((float)(GRAVITY_CNST) * masseAttracteur * masseOrbiteur) / Mathf.Pow (Vector3.Distance (orbiteur.transform.position, attracteur.transform.position), 2f)) * Time.deltaTime;
-
-				rb.AddForce (direction * force);
+				force = Time.deltaTime * 8e-10f * ((GRAVITY_CNST) * masseAttracteur * masseOrbiteur) / Mathf.Pow (Vector3.Distance (orbiteur.transform.position, attracteur.transform.position), 2f);
+				Debug.Log (force);
+				rb.AddForce (direction * (float) force, ForceMode.Acceleration);
 			}
 		}
 	}
