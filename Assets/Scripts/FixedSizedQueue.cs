ec5f94c020f96e8e7e81a5c1575bc00d5d122b12
PabloPetit <pablopetit94@gmail.com>
2016-11-05 16:36:09 +0100

    COMMIT

--- Assets/Scripts/FixedSizedQueue.cs
+++ Assets/Scripts/FixedSizedQueue.cs
@@ -4,27 +4,30 @@ using System.Collections.Generic;
 
 
 public class FixedSizedQueue<T> : Queue<T>
+{
+
+	/*
+	private readonly object syncObject = new object();
+
+	public int Size { get; private set; }
+
+	public FixedSizedQueue(int size)
 	{
-		private readonly object syncObject = new object();
+		Size = size;
+	}
 
-		public int Size { get; private set; }
-
-		public FixedSizedQueue(int size)
+	public new void Enqueue(T obj)
+	{
+		base.Enqueue(obj);
+		lock (syncObject)
 		{
-			Size = size;
-		}
-
-		public new void Enqueue(T obj)
-		{
-			base.Enqueue(obj);
-			lock (syncObject)
+			while (base.Count > Size)
 			{
-				while (base.Count > Size)
-				{
-					T outObj;
-					base.Dequeue(out outObj);
-				}
+				T outObj;
+				base.Dequeue(outObj);
 			}
 		}
 	}
+	*/
 }
+
--- Assets/Systems/ApplyBoostSystem.cs
+++ Assets/Systems/ApplyBoostSystem.cs
@@ -20,32 +20,14 @@ public class ApplyBoostSystem : FSystem {
 			Flames flames = go.GetComponent<Flames> ();
 			Masse masse = prop.GetComponent<Masse> ();
 
-<<<<<<< HEAD
 			if (prop.isOn && prop.currentFuel > 0f) {
-=======
-			Slider thrustSlider = (Slider) prop.sliders.GetComponentsInChildren<Slider>()[0];
-			Slider fuelSlider = (Slider) prop.sliders.GetComponentsInChildren<Slider>()[1];
-
-			if (prop.isOn && prop.carburant>0){
-				// on applique la force sur la fusée
-				Rigidbody rb = go.GetComponent<Rigidbody>();
-				currentThrust = prop.maxThrust * thrustSlider.value * 0.01f; // on lit le pourcentage de poussée à appliquer 
->>>>>>> origin/master
 
 				Vector3 force = prop.target.transform.up * prop.currentThrust * 1000f * 50f; //On est en tonne
 				Rigidbody rb = prop.target.GetComponent<Rigidbody> ();
 				rb.AddForce (force);
-
-<<<<<<< HEAD
 				prop.currentFuel = Mathf.Max(prop.currentFuel - prop.currentThrust / prop.maxThrust * prop.consumption * Time.fixedDeltaTime, 0f);
 			}
-=======
-				//go.transform.rotation = Quaternion.AngleAxis(orientationSlider.value, Vector3.forward);
-				Vector3 force = currentThrust * Time.fixedDeltaTime * Vector3.up;
 
-				//force = go.transform.rotation * force;
-				Debug.Log (force);
->>>>>>> origin/master
 
 			masse.mass = prop.emptyMass + prop.currentFuel;
 
