b39ebeb97a1e71d60af53f66217c7641e24e151e
PabloPetit <pablopetit94@gmail.com>
2016-11-05 17:04:06 +0100

    Commit

--- Assets/Scripts/FixedSizedQueue.cs
+++ Assets/Scripts/FixedSizedQueue.cs
@@ -5,9 +5,7 @@ using System.Collections.Generic;
 
 public class FixedSizedQueue<T> : Queue<T>
 {
-
 	/*
-	private readonly object syncObject = new object();
 
 	public int Size { get; private set; }
 
@@ -19,15 +17,16 @@ public class FixedSizedQueue<T> : Queue<T>
 	public new void Enqueue(T obj)
 	{
 		base.Enqueue(obj);
-		lock (syncObject)
+
 		{
 			while (base.Count > Size)
 			{
-				T outObj;
+				T outObj = base.Peek ();
 				base.Dequeue(outObj);
 			}
 		}
 	}
+
 	*/
 }
 
--- Assets/Systems/MonitorTextSystem.cs
+++ Assets/Systems/MonitorTextSystem.cs
@@ -12,6 +12,12 @@ public class MonitorTextSystem : FSystem {
 	// Use this to update member variables when system resume.
 	// Advice: avoid to update your families inside this function.
 	protected override void onResume(int currentFrame){
+
+		foreach (GameObject go in monitors){
+			MonitorText m = go.GetComponent<MonitorText> ();
+			m.lastPosition = m.target.transform.position;
+			m.lastSpeed = 0f;
+		}
 	}
 
 	// Use to process your families.
@@ -22,6 +28,17 @@ public class MonitorTextSystem : FSystem {
 			Text text = go.GetComponent<Text> ();
 			m.timer += Time.deltaTime;
 
+			float speed = Vector3.Distance (m.target.transform.position, m.lastPosition) / Time.deltaTime * 3.6f;
+			float g = Time.deltaTime * (speed - m.lastSpeed) * 3.6f * 9.81f * 2f;
+			float alt = Vector3.Distance (new Vector3 (0f, -6371000, 0f, 0f), m.target.transform.position);
+			alt += 6371000;
+			m.lastPosition = m.target.transform.position;
+			m.lastSpeed = speed;
+
+			string tex = "T : +"+(int)m.timer+"\n";
+			tex += "Speed : " + (int)speed+" km/h"+"\n"; 
+			tex += "Acceleration : " + (int)g+" G"+"\n";
+			tex += "Altitude : " + (int)alt+" m"+"\n";
 
 
 		}
