e65b41024382ab01fcff68631cfb38abc7b1a40f
Daniel Antunes Costa Gonçalves <dantunescost92@gmail.com>
2016-11-09 18:12:50 +0100

    orientation de la fusée, slider non intégré

---
+++ Assets/Components/HUD/OrientationHUDComponent.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class OrientationHUDComponent : MonoBehaviour {
+	// Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
+	public GameObject shuttle;
+	public GameObject earth;
+}
