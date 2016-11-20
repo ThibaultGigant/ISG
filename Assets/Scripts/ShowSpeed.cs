da5efa68ea535fcf9bfc89ebf258dbc40a4a4e28
Thibault Gigant <thibault.gigant@gmail.com>
2016-11-20 10:55:40 +0100

    Revert "Clean"
    This reverts commit 999de82025af71d5c211613153168e91ab8e6f17.

---
+++ Assets/Scripts/ShowSpeed.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowSpeed : MonoBehaviour {
+
+	Vector3 lastPos;
+	float lastSpeed;
+	float time;
+
+
+	// Use this for initialization
+	void Start () {
+		lastPos = gameObject.transform.position;
+		lastSpeed = 0;
+		time = 0f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		time += Time.fixedDeltaTime;
+
+		float speed = Vector3.Distance (gameObject.transform.position, lastPos) / Time.fixedDeltaTime * 3.6f ;
+
+		//float g = Time.fixedDeltaTime * (speed - lastSpeed) * 3.6f * 9.81f * 2f;// Vas savoir pourquoi on doit mettre x2
+		float g = (speed - lastSpeed)  / Time.fixedDeltaTime / 9.81f / 2f;
+		Debug.Log ("T = "+((int)time)+" "+gameObject.name+" speed : "+((int)speed)+ "km/h\nAcceleration : "+g+" G");
+
+		lastPos = gameObject.transform.position;
+
+		lastSpeed = speed;
+	}
+}
