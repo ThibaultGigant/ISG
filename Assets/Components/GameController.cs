9de6fe47f7c458fa5b38cedf49532a9cf57f70ed
PabloPetit <pablopetit94@gmail.com>
2016-11-24 13:17:02 +0100

    GameManager

---
+++ Assets/Components/GameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject target;
+	public TrajectoryGenerator generator;
+
+	public float maxTrajectoryDistance;
+
+
+	public float GAlertThreshold;
+	public float GFailThreshold;
+	public float GFailThresholdDuration;
+
+	public float DragAlertThreshold;
+	public float DragFailThreshold;
+
+	public float MaxCollisionSpeed;
+
+	public float acceleration;
+	public float speed;
+	public float groundSpeed;
+	public float altitude;
+	public float drag;
+
+
+	public float orientation;
+	public float earthOrientation;
+
+
+}
