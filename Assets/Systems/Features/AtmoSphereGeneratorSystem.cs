044dd744921d8af0329b6c8b5a33d9066111b80d
PabloPetit <pablopetit@gmail.com>
2016-11-26 22:07:45 +0100

    Mooooooon

--- Assets/Systems/Features/AtmoSphereGeneratorSystem.cs
+++ Assets/Systems/Features/AtmoSphereGeneratorSystem.cs
@@ -1,36 +1,42 @@
 ﻿using UnityEngine;
 using FYFY;
 
-public class AtmoSphereGeneratorSystem : FSystem {
+public class AtmoSphereGeneratorSystem : FSystem
+{
 
 
-	Family generator = FamilyManager.getFamily (new AllOfComponents(typeof(AtmoSphereGeneratorComponent)));
+	Family generator = FamilyManager.getFamily (new AllOfComponents (typeof(AtmoSphereGeneratorComponent)));
 
 
 
-	public AtmoSphereGeneratorSystem(){
-		foreach(GameObject go in generator){
+	public AtmoSphereGeneratorSystem ()
+	{
+		foreach (GameObject go in generator) {
 			AtmoSphereGeneratorComponent atmo = go.GetComponent<AtmoSphereGeneratorComponent> ();
 
-			for(int i=0; i < 100/*PhysicsConstants.atmosphereEnd / atmo.precision*/;i++){
+			for (int i = 0; i < 100/*PhysicsConstants.atmosphereEnd / atmo.precision*/; i++) {
 
 				GameObject sphere = GameObjectManager.instantiatePrefab ("AtmoshereUnit");
 				sphere.transform.localScale = Vector3.one * 100f * PhysicsConstants.altitudeOffset + Vector3.one * 10000f * i;//* PhysicsConstants.atmosphereEnd / atmo.precision;
+				sphere.transform.Rotate (Vector3.one * Random.Range (0, 360));
 			}
 
 
 		}
 	}
 
-	protected override void onPause(int currentFrame) {
+	protected override void onPause (int currentFrame)
+	{
 	}
 
 	// Use this to update member variables when system resume.
 	// Advice: avoid to update your families inside this function.
-	protected override void onResume(int currentFrame){
+	protected override void onResume (int currentFrame)
+	{
 	}
 
 	// Use to process your families.
-	protected override void onProcess(int familiesUpdateCount) {
+	protected override void onProcess (int familiesUpdateCount)
+	{
 	}
 }
