e84438eb4c734e4a5e409db401e4e6e1e478a378
PabloPetit <pablopetit94@gmail.com>
2016-11-26 11:45:04 +0100

    Atmosphere tout pourri

---
+++ Assets/Components/AtmoSphereGeneratorComponent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class AtmoSphereGeneratorComponent : MonoBehaviour {
+	
+
+	public float precision;
+
+
+}
---
+++ Assets/Systems/Features/AtmoSphereGeneratorSystem.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using FYFY;
+
+public class AtmoSphereGeneratorSystem : FSystem {
+
+
+	Family generator = FamilyManager.getFamily (new AllOfComponents(typeof(AtmoSphereGeneratorComponent)));
+
+
+
+	public AtmoSphereGeneratorSystem(){
+		foreach(GameObject go in generator){
+			AtmoSphereGeneratorComponent atmo = go.GetComponent<AtmoSphereGeneratorComponent> ();
+
+			for(int i=0; i < 100/*PhysicsConstants.atmosphereEnd / atmo.precision*/;i++){
+
+				GameObject sphere = GameObjectManager.instantiatePrefab ("AtmoshereUnit");
+				sphere.transform.localScale = Vector3.one * 100f * PhysicsConstants.altitudeOffset + Vector3.one * 10000f * i;//* PhysicsConstants.atmosphereEnd / atmo.precision;
+			}
+
+
+		}
+	}
+
+	protected override void onPause(int currentFrame) {
+	}
+
+	// Use this to update member variables when system resume.
+	// Advice: avoid to update your families inside this function.
+	protected override void onResume(int currentFrame){
+	}
+
+	// Use to process your families.
+	protected override void onProcess(int familiesUpdateCount) {
+	}
+}
