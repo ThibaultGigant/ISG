801e85f957cc8c294a091b2ee8dbb16fd9349721
PabloPetit <pablopetit@gmail.com>
2016-11-30 23:27:25 +0100

    Menu et mission 4 (pas encore bon)

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour
+{
+
+	public int scene;
+
+	public void Load ()
+	{
+		Application.LoadLevel (scene);
+	}
+}
