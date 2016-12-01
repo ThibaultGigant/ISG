30abd843297f7fbc72730fa283a72de37b8877fd
Keithdae <keithdae@gmail.com>
2016-12-01 17:40:28 +0100

    Nouvelle gestion temps

---
+++ Assets/Components/TimeScaleHandler.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TimeScaleHandler : MonoBehaviour {
+
+	[HideInInspector]
+	public int currentTimeScale = 0;
+	public List<float> timeScales = new List<float>(){1.0f,2.0f,5.0f,10.0f,20.0f,50.0f};
+
+}
